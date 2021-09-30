50733479297dce48debe947b51a741d9d0453f4f
31oli31 <oli31.d@gmail.com>
2021-09-30 15:09:01 +0200

    test

--- cv-app/src/app/components/Wave/Wave.jsx
+++ cv-app/src/app/components/Wave/Wave.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+
 import "./Wave.scss";
 import w from "../../assets/wave2.svg";
 const Wave = () => {
+
   return (
     <div className="parent">
-      <img src={w} className="waveImg"/>
+      <img src={w} className="waveImg" />
       <div className="overlay">
         <div className="headerText">OLIVER DIRR</div>
       </div>
